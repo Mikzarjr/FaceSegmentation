df371485b7d325c820830c96036618095f45d7bd
Mikzarjr <107720964+Mikzarjr@users.noreply.github.com>
2024-07-27 23:23:56 +0300

    Delete statr.py

--- statr.py
+++
@@ -1,8 +0,0 @@
-from Pipeline.Config import *
-from Pipeline.Segmentation import single_image_segmentation
-
-image_path = f"{IMGS_DIR}/img1.jpeg"
-
-S = single_image_segmentation(image_path)
-S.Segment()
-
