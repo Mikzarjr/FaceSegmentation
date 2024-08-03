55f2bd4a280baacfe0778e4f4788fda1969c21bb
Mikzarjr <mikzar.jr@gmail.com>
2024-08-03 22:53:05 +0300

    added functionality

--- Pipeline/Annotation.py
+++ Pipeline/Annotation.py
@@ -1,22 +1,21 @@
 from FaceSegmentation.Pipeline.Config import *
-from FaceSegmentation.Tools import *
+from FaceSegmentation.Pipeline.Tools import *
 
 
 class CreateJson:
-    def __init__(self, image_path, json_name):
+    def __init__(self, image_path):
         self.image_path = image_path
         self.image_dir = GetImageDir(image_path)
         self.image_name = GetImageName(image_path)
         self.json = None
-        self.json_name = json_name
 
     def CheckJson(self):
         if self.json is None:
-            print(f"There is no json annotation for {self.image_name}")
-            print(f"Run CreateJsonAnnotation to create one")
+            print(f"There is no json annotation for '{self.image_name}'")
+            print("Run 'CreateJsonAnnotation' to create one")
         else:
             coco = COCO(f'{MAIN_DIR}/{self.json}.json')
-            img_dir = {self.image_dir}
+            img_dir = self.image_dir
             image_id = 0
             img = coco.imgs[image_id]
             image = np.array(Image.open(os.path.join(img_dir, img['file_name'])))
@@ -27,5 +26,11 @@ class CreateJson:
             coco.showAnns(anns)
 
     def CreateJsonAnnotation(self):
-        print("CREATE JSON METHOD EXECUTED")
-        self.json = self.json_name
+        with open('ConstantData.json', 'r') as f:
+            coco_data = json.load(f)
+
+        json_name = "coco_annotations"
+        with open(f"{json_name}.json", "w") as json_file:
+            json.dump(coco_data, json_file, indent=4)
+
+        self.json = json_name
