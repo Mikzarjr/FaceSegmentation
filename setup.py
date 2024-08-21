d76a201f7ea0df670020f60945c35c1ba6df2250
Mikzarjr <mikzar.jr@gmail.com>
2024-08-21 17:25:33 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1.1',
     packages=find_packages(include=['docks', 'src']),
     description='Face segmentation with YOLO on dataset labelled with CLIP, Grounding DINO and Grounding SAM',
-    python_requires='>=3.6',
+    python_requires='>=3.10',
     install_requires=[
         'autodistill',
         'autodistill_clip',
