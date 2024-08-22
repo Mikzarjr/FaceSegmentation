694e813c2acd16f53d0458aa5f3ba41ed7761543
Mikzarjr <mikzar.jr@gmail.com>
2024-08-22 17:48:51 +0300

    maindir rework

--- demo/SegmentationOnly.py
+++ demo/SegmentationOnly.py
@@ -1,7 +1,11 @@
 from FaceSegmentation.Segmentation.Segmentation import FaceSeg
 
-image_path = f"/docks/TestImages/img1.jpeg"
+# image_path = f"/docks/TestImages/img1.jpeg"
+#
+# S = FaceSeg(image_path)
+# Masks = S.Segment
+# S.SaveMasks()
 
-S = FaceSeg(image_path)
-Masks = S.Segment
-S.SaveMasks()
+from FaceSegmentation.src.utils import MAIN_DIR
+
+print(MAIN_DIR)
